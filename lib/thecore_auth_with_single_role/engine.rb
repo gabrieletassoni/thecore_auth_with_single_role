57c78a98a3fde2d55736cba815ae3fa341e75c34
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:28:33 +0200

    migration initializer for engine cleanup

--- lib/thecore_auth_with_single_role/engine.rb
+++ lib/thecore_auth_with_single_role/engine.rb
@@ -2,7 +2,7 @@ module ThecoreAuthWithSingleRole
   class Engine < ::Rails::Engine
 
     initializer 'thecore_auth_with_single_role.add_to_migrations' do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
